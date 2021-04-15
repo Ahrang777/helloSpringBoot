fc934be0a0a72514a066a559407e7928f5d8909c
JunginPark4364 <wjddlsee@gmail.com>
2021-04-16 05:14:26 +0900

    first spring boot project

---
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package kr.ac.hansung.cse.hellospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "index"; // view 관련된 template name
+        // 예전엔 jsp 를 썼는데 Thymeleaf 쓸 생각
+    }
+
+}
